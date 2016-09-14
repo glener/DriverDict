0f21c7a8b5e23054223a0e68add32e98bd36a0a1
glener <again@Mac-mini.local>
2016-09-15 01:56:28 +0800

    驾考

--- 75AG驾校助手/ViewController.m
+++ 75AG驾校助手/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "selectView.h"
+#import "FirstViewController.h"
+#import "SubjectTwoViewController.h"
+#import "WebViewController.h"
+#import "WebViewController2.h"
 
 @interface ViewController ()
+
+@property (strong,nonatomic)  selectView *selectView;
+@property (weak, nonatomic) IBOutlet UIButton *selctBtn;
 
 @end
 
@@ -16,12 +24,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.selectView = [[selectView alloc] initWithFrame:self.view.frame andBtn:self.selctBtn];
+    self.selectView.alpha = 0;
+    [self.view addSubview:self.selectView];
+    
 }
+
+//选择车型
+- (IBAction)click:(UIButton *)sender {
+    [UIView animateWithDuration:0.3 animations:^{
+        self.selectView.alpha = 1;
+    }];
+}
+
+//跳转科目一
+- (IBAction)ClassOne:(id)sender {
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] init];
+    item.title = @"";
+    self.navigationItem.backBarButtonItem = item;
+    [self.navigationController pushViewController:[[FirstViewController alloc] init] animated:YES];
+}
+- (IBAction)ClassTwo:(id)sender {
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] init];
+    item.title = @"返回";
+    self.navigationItem.backBarButtonItem = item;
+    [self.navigationController pushViewController:[[SubjectTwoViewController alloc] init] animated:YES];
+}
+- (IBAction)ClassThree:(id)sender {
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] init];
+    item.title = @"返回";
+    self.navigationItem.backBarButtonItem = item;
+    [self.navigationController pushViewController:[[SubjectTwoViewController alloc] init] animated:YES];
+}
+- (IBAction)ClassFour:(id)sender {
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] init];
+    item.title = @"";
+    self.navigationItem.backBarButtonItem = item;
+    [self.navigationController pushViewController:[[FirstViewController alloc] init] animated:YES];
+}
+
+- (IBAction)BaomingXuzhi:(id)sender {
+//    UIBarButtonItem *item = [[UIBarButtonItem alloc] init];
+//    item.title = @"返回";
+//    self.navigationItem.backBarButtonItem = item;
+    WebViewController2 *web2 = [[WebViewController2 alloc] init];
+//    [self showViewController:web2 sender:nil];
+    [self.navigationController pushViewController:web2 animated:YES];
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
